43cdfabb5ec4c9aeb721dee4f393110de7f7bc39
angel.rogu017@gmail.com <angel.rogu017@gmail.com>
2021-12-16 00:07:22 -0600

    Push final

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -19,7 +19,6 @@ public class App
         System.out.println( "Proyecto Final de Sistemas Web" );
 
         staticFiles.location("/");
-        port(getHerokuAssignedPort());
 
 
         options("/*", (request, response) -> {
@@ -125,11 +124,4 @@ public class App
 
     }
 
-   static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
 }
